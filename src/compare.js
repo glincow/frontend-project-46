9c70d72c101fc0ec396b92961ba4988625c94974
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2024-11-19 21:07:04 +0300

    Refactore compare

--- src/compare.js
+++ src/compare.js
@@ -3,25 +3,29 @@ import _ from 'lodash';
 const compare = (json1, json2) => {
   const json1sorted = _.pick(json1, Object.keys(json1).sort());
   const json2sorted = _.pick(json2, Object.keys(json2).sort());
-  const result = [];
-  for (const [key, value] of Object.entries(json1sorted)) {
+  const result = Object.keys(json1sorted).reduce((acc, key) => {
+    const value = json1sorted[key];
     if (Object.hasOwn(json2sorted, key)) {
       if (json2sorted[key] === value) {
-        result.push(`  ${key}: ${value}`);
+        acc.push(`  ${key}: ${value}`);
       } else {
-        result.push(`- ${key}: ${value}`);
-        result.push(`+ ${key}: ${json2sorted[key]}`);
+        acc.push(`- ${key}: ${value}`);
+        acc.push(`+ ${key}: ${json2sorted[key]}`);
       }
     } else {
-      result.push(`- ${key}: ${value}`);
+      acc.push(`- ${key}: ${value}`);
     }
-  }
-  for (const [key, value] of Object.entries(json2sorted)) {
+    return acc;
+  }, []);
+
+  const finalResult = Object.keys(json2sorted).reduce((acc, key) => {
     if (!Object.hasOwn(json1sorted, key)) {
-      result.push(`+ ${key}: ${value}`);
+      acc.push(`+ ${key}: ${json2sorted[key]}`);
     }
-  }
-  const sortedResult = result.toSorted((a, b) => {
+    return acc;
+  }, result);
+
+  const sortedResult = finalResult.toSorted((a, b) => {
     if (a.substring(2).split(':')[0] < b.substring(2).split(':')[0]) {
       return -1;
     } if (a.substring(2).split(':')[0] > b.substring(2).split(':')[0]) {
