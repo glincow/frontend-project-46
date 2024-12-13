ae2addb9e4b26fd7d24a5e5f86474ab1b4e9f0ba
Anatolii Siuniaev <a.a.syunyaev@gmail.com>
2024-12-13 16:44:55 +0300

    Added library

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import compare from './src/compare.js';
 
-const genDiff = (path1, path2, format) => compare(path1, path2, format);
+const genDiff = (path1, path2, format = 'stylish') => compare(path1, path2, format);
 
 export default genDiff;
